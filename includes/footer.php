09cf28f98c6311b92577e6488c471da48e77bfcf
Vinay-Munjal <84566067+Vinay-Munjal@users.noreply.github.com>
2021-10-20 16:41:23 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <section id="footer" class="footer">
-© 2021 NEW. All rights reserved.
+© 2021 PC ONLY FORUM. All rights reserved.
 </section>
 <!-- JQuery Starts -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -12,5 +12,8 @@
 <!-- Custom JS Starts -->
 <script src="js/script.js"></script>
 <!-- Custom JS Ends -->
+<!-- For Tags Starts -->
+<script src="http://bootstrap-tagsinput.github.io/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js"></script>
+<!-- For Tags Ends -->
 </body>
 </html>
